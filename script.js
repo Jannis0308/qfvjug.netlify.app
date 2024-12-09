06b1b7fce58a8cd8dc50eda9922265d5f6a22ad6
Jannis0308 <130745442+Jannis0308@users.noreply.github.com>
2024-12-09 16:01:33 +0000

    Update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function sayHello() {
   alert("Hallo Welt!");
 }
+function toggleVisibility() {
+  const section = document.getElementById('video-upload-section');
+
+  // Überprüfen, ob das Element sichtbar ist
+  if (section.style.display === 'none' || section.style.display === '') {
+    // Wenn es versteckt ist, anzeigen
+    section.style.display = 'block';
+  } else {
+    // Wenn es sichtbar ist, ausblenden
+    section.style.display = 'none';
+  }
+}
